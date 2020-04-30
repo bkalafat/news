ef451a77ee57825d8c53d5d6c4dddd8927161214
bkalafat <kalafatburak@gmail.com>
2020-04-30 16:03:58 +0300

    feat: Create news app.

---
+++ src/components/News.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Slider from './Slider'
+
+function News () {
+    return (
+        <div>NewsPage
+            <Slider></Slider>
+        </div>
+
+    )
+}
+
+export default News;
---
+++ src/components/Slider.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Slider () {
+    return (
+        <div className='Slider'>Slider</div>
+    )
+}
+
+export default Slider
