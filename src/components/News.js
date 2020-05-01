d1d0996e2bdd9fccf034bbb03d576ce91abbf2cb
bkalafat <kalafatburak@gmail.com>
2020-05-02 02:17:40 +0300

    style: Resize slider for mobile or medium screen.

--- src/components/News.js
+++ src/components/News.js
@@ -1,21 +1,31 @@
 import React from "react";
-import Slider from "./Slider"
-import SubNews from "./SubNews"
-import newsList from "../utils/dummyData"
-import * as constant from "../utils/constant"
-
+import Slider from "./Slider";
+import SubNews from "./SubNews";
+import newsList from "../utils/dummyData";
+import * as constant from "../utils/constant";
 
 const News = () => {
-
-  const sliderNewsList = newsList.filter(news => news.isActive && news.type === constant.NEWS_TYPE).sort(function(a, b){return a.priority - b.priority}).slice(0,10);
-  const subNewsList = newsList.filter(news => news.isActive && news.type === constant.SUB_NEWS_TYPE).sort(function(a, b){return a.priority - b.priority}).slice(0,9);
+  const sliderNewsList = newsList
+    .filter((news) => news.isActive && news.type === constant.NEWS_TYPE)
+    .sort(function (a, b) {
+      return a.priority - b.priority;
+    })
+    .slice(0, 10);
+  const subNewsList = newsList
+    .filter((news) => news.isActive && news.type === constant.SUB_NEWS_TYPE)
+    .sort(function (a, b) {
+      return a.priority - b.priority;
+    })
+    .slice(0, 9);
 
   return (
     <div >
-      <Slider newsList={sliderNewsList} ></Slider>
-      <SubNews newsList ={subNewsList} ></SubNews>
+      <div className="col-md-8 col-xl-8 center-item">
+        <Slider newsList={sliderNewsList}></Slider>
+      </div>
+      <SubNews newsList={subNewsList}></SubNews>
     </div>
   );
-}
+};
 
 export default News;
--- src/components/NewsDetail.js
+++ src/components/NewsDetail.js
@@ -9,7 +9,7 @@ const NewsDetail = () => {
     <div className="newsDetail">
       <h1 className="spaceAround" >{news.caption}</h1>
       <p className="lead spaceAround">{news.summary}</p>
-      <img className="spaceAround detailImg" src={news.imgPath} alt={news.imgAlt} />
+      <img className="col-md-6 col-xs-12 col-sm-12 detailImg" src={news.imgPath} alt={news.imgAlt} />
       <p className="spaceAround" >{news.content}</p>
     </div>
   );
