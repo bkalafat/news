419e6e4fd95f4880a67fa399eedd87c5ac1be8e9
bkalafat <kalafatburak@gmail.com>
2020-05-10 02:06:14 +0300

    feat: Add category, caption and priority inputs.

--- src/components/Admin/NewsEditor.js
+++ src/components/Admin/NewsEditor.js
@@ -4,25 +4,36 @@ import { Form, Button } from "react-bootstrap";
 const NewsEditor = () => {
   const [type, setType] = useState("news");
   const [category, setCategory] = useState("");
+  const [caption, setCaption] = useState("");
+  const [summary, setSummary] = useState("");
   const [isActive, setIsActive] = useState(true);
+  const [priority, setPriority] = useState(888);
 
   const handleSubmit = (event) => {
-    alert("Seçilen Tip " + type + " Yazılan Kategori " + category + " Aktif mi " + isActive);
+    alert(
+      "Seçilen Tip " +
+        type +
+        " Yazılan Kategori " +
+        category +
+        " Aktif mi " +
+        isActive
+    );
     event.preventDefault();
   };
 
   return (
     <div className="centerFlex">
       <Form onSubmit={handleSubmit} className="col-md-10 col-xl-10">
-        <Form.Group controlId="exampleForm.ControlInput1">
+        <Form.Group>
           <Form.Label>Kategori</Form.Label>
           <Form.Control
-
             value={category}
             onChange={(e) => setCategory(e.target.value)}
-            placeholder="Kategori"
+            placeholder="Kategori giriniz"
           />
         </Form.Group>
+
+
         <Form.Group>
           <Form.Label>Tip</Form.Label>
           <Form.Control
@@ -34,7 +45,31 @@ const NewsEditor = () => {
             <option value="subNews">Alt Haber</option>
           </Form.Control>
         </Form.Group>
-        <Form.Group controlId="exampleForm.ControlSelect2">
+
+        <Form.Group>
+          <Form.Label>Başlık</Form.Label>
+          <Form.Control
+            value={caption}
+            onChange={(e) => setCaption(e.target.value)}
+            placeholder="Başlık giriniz"
+          />
+        </Form.Group>
+
+        <Form.Group>
+          <Form.Label>Özet</Form.Label>
+          <Form.Control
+            value={summary}
+            onChange={(e) => setSummary(e.target.value)}
+            as="textarea" rows="2"
+          />
+        </Form.Group>
+
+        <Form.Group>
+          <Form.Label>İçerik</Form.Label>
+          <Form.Control as="textarea" rows="5" />
+        </Form.Group>
+
+        <Form.Group>
           <Form.Label>Durum</Form.Label>
           <Form.Control
             value={isActive}
@@ -45,10 +80,15 @@ const NewsEditor = () => {
             <option value={false}>Pasif</option>
           </Form.Control>
         </Form.Group>
+
         <Form.Group>
-          <Form.Label>İçerik</Form.Label>
-          <Form.Control as="textarea" rows="3" />
+          <Form.Label>Öncelik</Form.Label>
+          <Form.Control
+            value={priority}
+            onChange={(e) => setPriority(e.target.value)}
+          />
         </Form.Group>
+
         <Button variant="primary" type="submit">
           Submit
         </Button>
