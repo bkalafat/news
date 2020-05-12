3d1ef8fd30804989cfb76e2eca15c0dc4b4757e4
bkalafat <kalafatburak@gmail.com>
2020-05-12 13:26:46 +0300

    feat: Create sample Uploader adapter

---
+++ src/components/Admin/ContentEditor.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import CKEditor from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import UploadAdapter from './UploadAdapter'
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <h2>Using CKEditor 5 build in React</h2>
+                <CKEditor
+                    editor={ ClassicEditor }
+                    data="<p>Hello from CKEditor 5!</p>"
+                    onInit={ editor => {
+
+                        editor.plugins.get("FileRepository").createUploadAdapter = function(loader) {
+                            return new UploadAdapter(loader);
+                         };
+
+                        console.log( 'Editor is ready to use!', editor );
+                    } }
+                    onChange={ ( event, editor ) => {
+                        const data = editor.getData();
+                        console.log( { event, editor, data } );
+                    } }
+                    onBlur={ ( event, editor ) => {
+                        console.log( 'Blur.', editor );
+                    } }
+                    onFocus={ ( event, editor ) => {
+                        console.log( 'Focus.', editor );
+                    } }
+                />
+            </div>
+        );
+    }
+}
+
+export default App;
