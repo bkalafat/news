b53daf12ff198be818419a82072cbb9c16093691
bkalafat <kalafatburak@gmail.com>
2020-05-22 16:02:43 +0300

    Revert "feat: Try to add server side rendering.

--- src/index.js
+++ src/index.js
@@ -1,26 +1,13 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
 import './index.css'
-import { hydrate } from 'react-dom'
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
 import App from './App'
-import newsApp from './reducers'
 import * as serviceWorker from './serviceWorker'
 
-// Grab the state from a global variable injected into the server-generated HTML
-const preloadedState = window.__PRELOADED_STATE__
-
-// Allow the passed state to be garbage-collected
-delete window.__PRELOADED_STATE__
-
-// Create Redux store with initial state
-const store = createStore(newsApp, preloadedState)
-
-
-hydrate(
-  <Provider store={store}>
+ReactDOM.render(
+  <React.StrictMode>
     <App />
-  </Provider>,
+  </React.StrictMode>,
   document.getElementById('root')
 )
 
