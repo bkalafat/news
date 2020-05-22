c7f60fb2f552d511a3c87f1edab34c72be2e0c06
bkalafat <kalafatburak@gmail.com>
2020-05-22 15:47:55 +0300

    feat: Try to add server side rendering. alpha

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
 import './index.css'
+import { hydrate } from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 import App from './App'
+import newsApp from './reducers'
 import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(
-  <React.StrictMode>
+// Grab the state from a global variable injected into the server-generated HTML
+const preloadedState = window.__PRELOADED_STATE__
+
+// Allow the passed state to be garbage-collected
+delete window.__PRELOADED_STATE__
+
+// Create Redux store with initial state
+const store = createStore(newsApp, preloadedState)
+
+
+hydrate(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 )
 
