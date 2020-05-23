b1d655dc0b68be76b6274169ad468eb8d7ae5e32
bkalafat <kalafatburak@gmail.com>
2020-05-23 23:48:17 +0300

    feat: Add is second page news property to News.

--- backend/newsApi/newsApi/Data/MockNewsService.cs
+++ backend/newsApi/newsApi/Data/MockNewsService.cs
@@ -122,8 +122,8 @@ namespace newsApi.Data
                 ImgAlt = "test haber img",
                 Content =
                     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi at nisi ex. Nullam et elit elementum, consequat risus ut, aliquet metus. Phasellus pharetra, urna non mollis auctor, dui erat fermentum lorem, sed egestas sem nisl ut augue. Duis vitae turpis non dui luctus congue. Donec iaculis, diam in consequat dapibus, tortor mauris rhoncus ex, sit amet rutrum augue arcu a lacus. Vestibulum porta, orci vitae ultrices blandit, dolor metus tristique lorem,eget placerat nisl nulla in turpis. Maecenas vel aliquam leo. Vivamus eleifend sapien vel mauris mollis imperdiet.",
-                Subjects = new[] {"Covid", "Türkiye"},
-                Authors = new[] {"Mustafa Çolakoğlu", "Burak Kalafat"},
+                Subjects = new[] { "Covid", "Türkiye" },
+                Authors = new[] { "Mustafa Çolakoğlu", "Burak Kalafat" },
                 CreateDate = DateTime.Parse("2020-04-23T18:25:43.511Z"),
                 UpdateDate = DateTime.Parse("2020-05-01T14:35:43.511Z"),
                 ExpressDate = DateTime.Parse("2020-05-01T14:35:43.511Z"),
--- backend/newsApi/newsApi/Data/NewsService.cs
+++ backend/newsApi/newsApi/Data/NewsService.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using MongoDB.Driver;
 using newsApi.Models;
 
