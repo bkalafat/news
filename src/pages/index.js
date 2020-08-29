b5350e98ed9f8d5869e33d053182fcb364f1ce96
bkalafat <kalafatburak@gmail.com>
2020-08-29 15:09:37 +0300

    feat: Set main page as ssg

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,9 @@
 import Content from "../components/Content"
 import Layout from "../components/Layout"
 import Head from 'next/head'
+import * as API from "../utils/api"
 
-function Index() {
+const Index = (props) =>  {
   return (
     <Layout>
       <Head>
@@ -21,9 +22,19 @@ function Index() {
         <meta property="twitter:title" content="Haberibul.com" />
         <meta property="twitter:description" content="Ulusal Spor Magazin Ekonomi Futbol Canlı Son Dakika haberlerini bulabileceğiniz haberi bul sayfası." />
       </Head>
-      <Content />
+      <Content newsList={props.newsList} />
     </Layout>
   )
+}
+
+export const getStaticProps = async () => {
+  const newsList = await API.getNewsList()
+  return {
+    revalidate: 5,
+    props: {
+      newsList
+    }
+  }
 }
 
 export default Index
