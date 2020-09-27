ab86735aff89cea2d6499dea5dc854f3dac984c5
bkalafat <kalafatburak@gmail.com>
2020-09-27 23:37:13 +0300

    feat: Fix nav bar and class name

--- src/components/Navigator.js
+++ src/components/Navigator.js
@@ -12,7 +12,7 @@ const Navigator = () => {
             style={{ width: 80, height: 50, marginTop: -7 }}
           />
         </a></Link>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button className="navbar-toggler" type="button" type="button" data-toggle="collapse" data-target="#navbarTop" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
