f2ec25d16addd4ddb38d3635afd561b67d5c8356
bkalafat <kalafatburak@gmail.com>
2020-12-26 02:20:21 +0300

    fix: Add googlesyndication only once

--- src/components/SquareAd.js
+++ src/components/SquareAd.js
@@ -3,7 +3,6 @@ const SquareAd = () => {
     <div
       dangerouslySetInnerHTML={{
         __html: `
-                <script async src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js'></script>
                 <ins class='adsbygoogle' style='display:block' data-ad-client='ca-pub-9881133041867885'
                   data-ad-slot='4914698997' data-ad-format='auto' data-full-width-responsive='true'>
                 </ins>
--- src/pages/[category]/[slug]/[id].js
+++ src/pages/[category]/[slug]/[id].js
@@ -53,7 +53,7 @@ const NewsDetail = (props) => {
             __html:
               "<div class='container content center-item  text-center'" +
               news.content +
-              "<script async src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js'></script><ins class='adsbygoogle' style='display:block' data-ad-client='ca-pub-9881133041867885' data-ad-slot='4914698997' data-ad-format='auto' data-full-width-responsive='true'></ins> <script> (adsbygoogle = window.adsbygoogle || []).push({}) </script> </div>"
+              " <ins class='adsbygoogle' style='display:block' data-ad-client='ca-pub-9881133041867885' data-ad-slot='4914698997' data-ad-format='auto' data-full-width-responsive='true'></ins> <script> (adsbygoogle = window.adsbygoogle || []).push({}) </script> </div>"
           }}
         />
         <div className='container content center-item  text-center'>
