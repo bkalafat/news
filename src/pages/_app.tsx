5c87a63bb49660424587ce7dbb1a5e9ac441aa10
bkalafat <kalafatburak@gmail.com>
2021-01-11 12:04:48 +0300

    revert: Remove performance log.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,15 +4,11 @@ import "slick-carousel/slick/slick.css"
 import "slick-carousel/slick/slick-theme.css"
 import { Provider } from 'next-auth/client'
 import "reflect-metadata";
-import type { AppProps, NextWebVitalsMetric } from 'next/app'
+import type { AppProps } from 'next/app'
 
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return <Provider session={pageProps.session}>
     <Component {...pageProps} />
   </Provider>
-}
-
-export function reportWebVitals(metric: NextWebVitalsMetric) {
-  console.log(metric)
 }
